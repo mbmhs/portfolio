f6c97cacd36898d4d4c6aee0a8f30d0fc29d35c1
mbmhs <197660177+mbmhs@users.noreply.github.com>
2025-02-04 22:03:17 +0530

    This is my work as a beginner.
    Let me know my mistake. so that i can grow.
    Help me grow.

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
