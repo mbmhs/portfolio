331117c0063126e5817ea0947f1a17614e70051f
mbmhs <mbmhs@users.noreply.github.com>
2025-02-07 20:26:48 +0530

    update the latest changes with respoinsivness, hamburger, contact_form, dark_mode,etc.

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('nav-menu');
-
-hamburger.addEventListener('click', () => {
-    navMenu.querySelector('ul').classList.toggle('active');
-});
